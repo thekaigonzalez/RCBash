14f9e7500c643d687a5dabb61bef97f2f68d2071
Kai Daniel Gonzalez <gkai70264@gmail.com>
2022-08-11 09:42:49 -0400

    Change dir for safety

--- download.sh
+++ download.sh
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 chmod +x Software/*/*
-cp Software/*/* /usr/local/bin/
+cp Software/*/* /usr/local/bin/rcbin
